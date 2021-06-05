bbc28d4863a33cf1c20a456d52aaea018bb38524
Shivani Vemula <vemulashivani2012@gmail.com>
2021-06-05 15:32:24 +0530

    third commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/dynamicRegistration",{
+mongoose.connect("mongodb+srv://shivani:shivani@cluster0.ztvwq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
